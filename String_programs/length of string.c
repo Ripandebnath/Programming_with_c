b3ce71d7083b532648e641c119300a8455e324c1
Ripan Debnath <52417263+Ripandebnath@users.noreply.github.com>
2022-06-22 22:25:08 +0530

    WAP to find the length of the string in c

---
+++ String_programs/length of string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char fname[20];
+    int count=0,i=0;
+    printf("Enter your name:-\n");
+    scanf("%[^\n]",&fname);
+    while(fname[i]!='\0')
+    {
+        count++;
+        i++;
+    }
+    printf("\nLength of the string is:-%d",count);
+    return 0;
+}
